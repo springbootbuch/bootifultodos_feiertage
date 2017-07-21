58a34292ab02ae35fed9236dc3751ef87db47b77
Michael J. Simons <michael@simons.ac>
2017-07-21 17:22:52 +0300

    Enable Eureka client

--- src/main/java/de/bootifultodos/feiertage/FeiertageApplication.java
+++ src/main/java/de/bootifultodos/feiertage/FeiertageApplication.java
@@ -18,15 +18,20 @@ package de.bootifultodos.feiertage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Michael J. Simons, 2016-11-30
  */
 @SpringBootApplication
 @EnableCaching
+@EnableEurekaClient
 public class FeiertageApplication {
 
 	public static void main(final String... args) {
-		SpringApplication.run(FeiertageApplication.class, args);
+		SpringApplication.run(
+			FeiertageApplication.class,
+			args
+		);
 	}
 }
